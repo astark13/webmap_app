a2935d86dc52f93c52febcb65f39aa6005ab5cd5
astark13 <adrianstark13@yahoo.com>
2020-11-22 15:55:55 +0200

    Altitude instead of name; different color popups

--- map1.py
+++ map1.py
@@ -2,14 +2,24 @@ import folium, pandas
 data=pandas.read_csv("Volcanoes.txt") # creates dataframe 
 lat=list(data["LAT"])                 # turns dataframe column into list
 lon=list(data["LON"])
-name=list(data["NAME"])
+elev=list(data["ELEV"])
+
+print(type(elev))
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000<=elevation<=3000:
+        return 'orange'
+    else:
+        return 'red'
 
 map = folium.Map(location=[48.77,-121.81],zoom_start=10, tiles="Stamen Terrain") # creates a map with a base location
 
 fg = folium.FeatureGroup(name="My Map") # creates a group of features(e.g. markers, polygons, etc) that cand be added to the base map
 
-for lt, ln, na in zip(lat,lon,name):
-    fg.add_child(folium.Marker(location=[lt,ln], popup=(na), icon=folium.Icon(color='green')))  # adds a marker with a location, message/popup, and color
+for lt, ln, el in zip(lat,lon,elev):
+    fg.add_child(folium.Marker(location=[lt,ln], popup=str(el)+"m", icon=folium.Icon(color=color_producer(el))))  # adds a marker with a location, message/popup, and color
 map.add_child(fg)
 
 map.save("Map1.html")
