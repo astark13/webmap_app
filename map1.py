91dd3a7ee2ff137212d84bcc52e593e963cbe0f5
astark13 <adrianstark13@yahoo.com>
2020-11-25 12:03:46 +0200

    Layers fgv, fpg redesingned to allow layer control

--- map1.py
+++ map1.py
@@ -14,15 +14,19 @@ def color_producer(elevation):
 
 map = folium.Map(location=[48.77,-121.81],zoom_start=10, tiles="Stamen Terrain") # creates a map with a base location
 
-fg = folium.FeatureGroup(name="My Map") # creates a group of features(e.g. markers, polygons, etc) that cand be added to the base map
+fgv = folium.FeatureGroup(name="Volcanoes") # creates a feature group layer that contains markers for volcanoes
 
 for lt, ln, el in zip(lat,lon,elev):
-    fg.add_child(folium.CircleMarker(location=[lt,ln], radius=6, popup=str(el)+"m", 
-    fill_color=color_producer(el), color='grey', fill_opacity=0.7))  # adds a point-layer containing markers with a location, message/popup, and color
+    fgv.add_child(folium.CircleMarker(location=[lt,ln], radius=6, popup=str(el)+"m", 
+    fill_color=color_producer(el), color='grey', fill_opacity=0.7))  # each marker is represented by a circle, showing the altitude and different colors based on the altitude
 
-# adds a polygon-layer that shows borders(areas) and based on data regarding population from the json assigns a color to each country
-fg.add_child(folium.GeoJson(open("world.json",encoding = "utf-8-sig").read(), style_function=lambda x:{'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+fgp = folium.FeatureGroup(name="Population") # creates a feature group layer that contains data about population
 
-map.add_child(fg)
+# the fgp layer shows borders(areas) and based on data regarding population from the json assigns a color to each country
+fgp.add_child(folium.GeoJson(open("world.json",encoding = "utf-8-sig").read(), style_function=lambda x:{'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+map.add_child(fgv)                    # adds the fgv layer to the map
+map.add_child(fgp)                    # adds the fgp layer to the map
+map.add_child(folium.LayerControl())  # creates a button in the upper right corner allowing you to disable/enable layers
 
 map.save("Map1.html")
